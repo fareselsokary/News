30ddd50b1ca716b0139a2183b81878ac79f84466
fareselsokary <fareselsokary11@gmail.com>
2019-08-15 08:25:10 +0200

    Initial Commit

---
+++ News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by fareselsokary on 8/15/19.
+//  Copyright © 2019 Fares. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
